f6dd7ac1222f99931710ffa4fe344b8ab04e89f8
Zayne Abraham <zayneabraham1@gmail.com>
2017-02-08 05:56:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+end
+
+#bark = Dog.new
+fido  = Dog.new
+
+fido.bark
+
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+
+end
+
+#bark = Dog.new
+ada  = Person.new
+
+ada.talk
+
+ada.walk
